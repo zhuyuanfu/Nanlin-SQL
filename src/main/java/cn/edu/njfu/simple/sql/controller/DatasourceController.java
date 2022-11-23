8d5922849a4cb49d6e5ad018ad73016a667a466d
Zhu Yuanfu <641561609@qq.com>
2022-11-23 18:08:12 +0800

    ~[metadata: added a temporary solution to cors policy interception]

--- src/main/java/cn/edu/njfu/simple/sql/controller/DatasourceController.java
+++ src/main/java/cn/edu/njfu/simple/sql/controller/DatasourceController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import cn.edu.njfu.simple.sql.model.CustomResponse;
 import cn.edu.njfu.simple.sql.service.DatasourceService;
 import io.swagger.annotations.ApiOperation;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/datasource")
 public class DatasourceController {
